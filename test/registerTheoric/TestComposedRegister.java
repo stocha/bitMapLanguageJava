1db7ebf738e169d2060f7defe1b970e5dc9c0ec8
DFI <dfi@dfidaali.com>
2015-01-14 01:14:55 +0100

    partiel. Verification sur regle 30

---
+++ test/registerTheoric/TestComposedRegister.java
@@ -0,0 +1,206 @@
+package registerTheoric;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jahan
+ */
+public class TestComposedRegister {
+    
+    public TestComposedRegister() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+    @Test
+    public void keep2(){      
+        ComposedRegister b=new ComposedRegister(2, () -> new T2BitReg());        
+        b.xor(b);        
+        assertEquals( "OO",RegisterUtilis.toString(b));        
+        b.setAt(0, 1);
+        assertEquals("XO",RegisterUtilis.toString(b) );
+        b.shr();
+        assertEquals("OX",RegisterUtilis.toString(b) );    
+         b.setAt(1, 0);
+         assertEquals( "OO",RegisterUtilis.toString(b));
+    }    
+
+    @Test
+    public void basicTest(){      
+        ComposedRegister b=new ComposedRegister(11, () -> new T2BitReg());        
+        b.xor(b);        
+        assertEquals( "OOOOOOOOOOO",RegisterUtilis.toString(b));        
+        b.setAt(0, 1);
+        assertEquals("XOOOOOOOOOO",RegisterUtilis.toString(b) );
+        b.shr();
+        assertEquals("OXOOOOOOOOO",RegisterUtilis.toString(b) );    
+        b.setAt(1, 0);
+        assertEquals( "OOOOOOOOOOO",RegisterUtilis.toString(b));
+    }
+    
+    
+    public void apply30On(IRegister i,IRegFactory fact){
+        IRegister res=fact.alloc();
+        IRegister l=fact.alloc();
+        IRegister c=fact.alloc();
+        IRegister r=fact.alloc();
+        IRegister v=fact.alloc();
+       
+        IRegister lc;
+        IRegister cc;
+        IRegister rc;
+               
+        
+        res.xor(res);
+        
+        c.cp(i);
+        l.cp(i);l.shl();
+        r.cp(i);r.shr();
+        
+        
+        
+        lc=RegisterUtilis.and(
+                RegisterUtilis.and(l, RegisterUtilis.not(c, fact), fact),
+                RegisterUtilis.not(c, fact)
+                , fact);
+        res.or(v);
+        
+        v.cp(i);
+        v.shr();
+        res.or(v);
+        
+        v.cp(i);
+        v.shl();
+        v.and(i);
+        res.or(v);
+        
+        v.cp(i);
+        res.or(v);
+        
+        v.cp(i);
+        v.shl();
+        res.or(v);        
+        
+        i.cp(res);
+    }
+    
+    @Test
+    public void rule30_basic1(){
+        IRegFactory fact=() -> new T2BitReg();
+        IRegister b=new ComposedRegister(11, fact);  
+        IRegister v=new ComposedRegister(11, fact);  
+        IRegister res=new ComposedRegister(11, fact);  
+        b.xor(b);        
+        assertEquals( "OOOOOOOOOOO",RegisterUtilis.toString(b));        
+        b.setAt(5, 1);
+        assertEquals("OOOOOXOOOOO",RegisterUtilis.toString(b) );
+        
+        IRegister i=b;
+        v.cp(i);
+        assertEquals("OOOOOXOOOOO",RegisterUtilis.toString(v) );
+        v.shr();
+        assertEquals("OOOOOOXOOOO",RegisterUtilis.toString(v) );
+        res.or(v);
+        assertEquals("OOOOOOXOOOO",RegisterUtilis.toString(res) );
+        
+        v.cp(i);
+        v.shl();
+        v.and(i);
+        res.or(v);
+        assertEquals("OOOOOOXOOOO",RegisterUtilis.toString(res) );
+        
+        v.cp(i);
+        res.or(v);
+        assertEquals("OOOOOXXOOOO",RegisterUtilis.toString(res) );
+        
+        v.cp(i);
+        v.shl();
+        res.or(v);      
+        assertEquals("OOOOXXXOOOO",RegisterUtilis.toString(res) );
+        
+        i.cp(res);
+        assertEquals("OOOOXXXOOOO",RegisterUtilis.toString(i) );
+        
+    }    
+    
+    
+    @Test
+    public void rule30_basic2(){
+        IRegFactory fact=() -> new T2BitReg();
+        IRegister b=new ComposedRegister(11, fact);  
+        IRegister v=new ComposedRegister(11, fact);  
+        IRegister res=new ComposedRegister(11, fact);  
+      
+        RegisterUtilis.inputString(b,"OOOOXXXOOOO",'X');
+        assertEquals("OOOOXXXOOOO",RegisterUtilis.toString(b) );
+        
+        IRegister i=b;
+        v.cp(i);
+        assertEquals("OOOOXXXOOOO",RegisterUtilis.toString(v) );
+        v.shr();
+        assertEquals("OOOOOXXXOOO",RegisterUtilis.toString(v) );
+        res.or(v);
+        assertEquals("OOOOOXXXOOO",RegisterUtilis.toString(res) );
+        
+        v.cp(i);
+        v.shl();
+        v.and(i);
+        assertEquals("OOOOXXOOOOO",RegisterUtilis.toString(v) );
+        res.or(v);
+        assertEquals("OOOOXXXXOOO",RegisterUtilis.toString(res) );
+        
+        
+    }       
+    
+    @Test
+    public void rule30(){      
+        
+        IRegFactory fact=() -> new T2BitReg();
+        ComposedRegister b=new ComposedRegister(11, fact);  
+        b.xor(b);        
+        assertEquals( "OOOOOOOOOOO",RegisterUtilis.toString(b));        
+        b.setAt(5, 1);
+        assertEquals("OOOOOXOOOOO",RegisterUtilis.toString(b) );
+        
+        apply30On(b, ()->{
+            return new ComposedRegister(11, fact);
+        });
+        assertEquals("OOOOXXXOOOO",RegisterUtilis.toString(b) );    
+        
+        apply30On(b, ()->{
+            return new ComposedRegister(11, fact);
+        });
+        assertEquals("OOOXXOOXOOO",RegisterUtilis.toString(b) );  
+        apply30On(b, ()->{
+            return new ComposedRegister(11, fact);
+        });        
+        assertEquals("OOXXOXXXXO",RegisterUtilis.toString(b) ); 
+    }    
+}
---
+++ test/registerTheoric/TestT2BitReg.java
@@ -0,0 +1,95 @@
+package registerTheoric;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jahan
+ */
+public class TestT2BitReg {
+    
+    public TestT2BitReg() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testIt(){
+        T2BitReg b=new T2BitReg();        
+        b.xor(b);        
+        assertEquals( "OO",RegisterUtilis.toString(b));        
+        b.setAt(0, 1);
+        assertEquals("XO",RegisterUtilis.toString(b) );
+        b.shr();
+        assertEquals("OX",RegisterUtilis.toString(b) );    
+        b.setAt(1, 0);
+        assertEquals( "OO",RegisterUtilis.toString(b));
+    }
+    
+    
+    @Test
+    public void logic(){
+        T2BitReg b=new T2BitReg();        
+        T2BitReg a=new T2BitReg(); 
+
+        RegisterUtilis.inputString(a,"OO" , 'X');
+        RegisterUtilis.inputString(b,"OO" , 'X');
+        a.or(b);
+        assertEquals( "OO",RegisterUtilis.toString(a));      
+        
+        RegisterUtilis.inputString(a,"OX" , 'X');
+        RegisterUtilis.inputString(b,"XO" , 'X');
+        a.or(b);
+        assertEquals( "XX",RegisterUtilis.toString(a));          
+        
+        
+         RegisterUtilis.inputString(a,"XO" , 'X');
+         a.shr();
+         assertEquals( "OX",RegisterUtilis.toString(a)); 
+         a.shr();
+         assertEquals( "OO",RegisterUtilis.toString(a)); 
+         
+         
+         RegisterUtilis.inputString(a,"OX" , 'X');
+         a.shl(a.size()-1);
+         assertEquals( "XO",RegisterUtilis.toString(a)); 
+         
+         
+        RegisterUtilis.inputString(a,"OO" , 'X');
+        RegisterUtilis.inputString(b,"XO" , 'X');
+        a.or(b);
+        assertEquals( "XO",RegisterUtilis.toString(a));          
+        
+    }    
+}
