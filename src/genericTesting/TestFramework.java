497702808119fe31e35aa2bdb1e62998b9d22762
denis <denis@JahanTab>
2015-01-19 16:38:29 +0100

    Creation d'un mini system de tests et diagnostique

---
+++ src/genericTesting/TestFramework.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genericTesting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author denis
+ */
+public class TestFramework {
+    static HashMap<String,Integer> res=new  HashMap<String,Integer>(1000);
+    static HashMap<String,Integer> tot=new  HashMap<String,Integer>(1000);
+    static HashMap<String,String> modelVal=new  HashMap<String,String>(1000);
+    static HashMap<String,String> actualVal=new  HashMap<String,String>(1000);
+    
+    
+    public static void assertEquals(String model,String result){
+        
+        String key="NOKEY";
+        try{
+            throw new RuntimeException();
+        }catch(Exception e){
+            int l=e.getStackTrace().length;
+            
+//            for(int i=0;i<l;i++){
+//                            StackTraceElement st=e.getStackTrace()[i];
+//                            key=st.getClassName()+"."+st.getMethodName()+"."+st.getLineNumber();
+//                            System.out.println("debut ps "+key);
+//            }
+            {
+            StackTraceElement st=e.getStackTrace()[1];
+            key=st.getClassName()+"."+st.getMethodName()+"."+st.getLineNumber();
+            }
+        }
+        
+        if(!res.containsKey(key)){
+            res.put(key,0);
+            tot.put(key, 0);
+        }
+        
+        tot.put(key, tot.get(key)+1);
+        
+        if(model.equals(result)){
+            res.put(key, res.get(key)+1);
+        }else{
+            modelVal.put(key, model);
+            actualVal.put(key, result);
+        }
+        
+    }
+    
+    public static void showResults(){
+        List<String> a=new  ArrayList<String>();
+        a.addAll(tot.keySet());
+        
+        Collections.sort(a);
+        
+        for(String s :a ){
+                    System.out.println(""+s+" "+res.get(s)+"/"+tot.get(s));
+                    
+                    if(res.get(s)!=tot.get(s)){
+                        System.out.println(""+modelVal.get(s));
+                        System.out.println(""+actualVal.get(s));
+                    }
+        }
+        
+
+    }
+}
