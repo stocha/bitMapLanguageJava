98bcac3364cfc40da300513adc98038f8c161d51
denis <denis@JahanTab>
2015-01-22 13:58:33 +0100

    debugage entrees sorties texte goban

--- src/L64p/vrac/L64fbase.java
+++ src/L64p/vrac/L64fbase.java
@@ -10,15 +10,15 @@ package L64p.vrac;
  * @author denis
  */
 public class L64fbase {
-    
-    public static final long RMASK=0x7f7f7f7f7f7f7f7fL;
-    public static final long LMASK=0xFEFEFEFEFEFEFEFEL;
-    
-    public static final long rule30(long mem){
+
+    public static final long RMASK = 0x7f7f7f7f7f7f7f7fL;
+    public static final long LMASK = 0xFEFEFEFEFEFEFEFEL;
+
+    public static final long rule30(long mem) {
         //(l xor (c or r))
-        long l=(mem>>>1 | mem<<63);
-        long r=(mem>>>63 | mem<<1);
-         return (l ^ (mem | r));
+        long l = (mem >>> 1 | mem << 63);
+        long r = (mem >>> 63 | mem << 1);
+        return (l ^ (mem | r));
     }
 
     public static final int getAt(long mem, int x, int y) {
@@ -35,6 +35,86 @@ public class L64fbase {
         mem |= (v << (63 - dec));
 
         return mem;
+    }
+
+    public static final long inputString(String model, int player) {
+        long res = 0;
+        int pos = 0;
+        player++;
+        boolean ignore = false;
+        for (int j = 0; j < 8; j++) {
+            for (int i = 0; i < 8; i++) {
+
+                int out = 0;
+
+                boolean found = false;
+
+                while (!found) {
+                    char in = model.charAt(pos++);
+                    found = true;
+                    switch (in) {
+                        case '-':
+                            out = 0;
+                            break;
+                        case 'X':
+                            out = 1;
+                            break;
+                        case 'O':
+                            out = 2;
+                            break;
+                        case '#':
+                            out = 3;
+                            break;
+                        case '<':
+                            ignore = true;
+                            break;
+                        case '>':
+                            ignore = false;
+                            found = false;
+                            break;
+                        default:
+                            found = false;
+                    }
+                    if (ignore) {
+                        found = false;
+                    }
+                }//while
+                //System.out.print(""+model.charAt(pos-1));
+                res=setAt(res, i, j, (out & player) | ((out & player) >>> 1));
+            }//For width
+        }//For height
+        return res;
+    }
+
+    public static final String outString(long mem0, long mem1) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<GOBAN>" + "\n");
+        for (int j = 0; j < 8; j++) {
+            for (int i = 0; i < 8; i++) {
+
+                int v = getAt(mem0, i, j) | (getAt(mem1, i, j) << 1);
+                char out = ' ';
+
+                switch (v) {
+                    case 0:
+                        out = '-';
+                        break;
+                    case 1:
+                        out = 'X';
+                        break;
+                    case 2:
+                        out = 'O';
+                        break;
+                    case 3:
+                        out = '#';
+                        break;
+                }
+                sb.append(out);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static final String outString(long mem, char t, char f) {
@@ -96,7 +176,7 @@ public class L64fbase {
     public static final long tool_lMask() {
         long mask = 0;
         for (int i = 0; i < 64; i++) {
-            if ((i &0x7) == 0) {
+            if ((i & 0x7) == 0) {
                 mask |= 1L << i;
             }
         }
--- src/L64p/vrac/Test_L64Bm_simpleFast.java
+++ src/L64p/vrac/Test_L64Bm_simpleFast.java
@@ -30,20 +30,46 @@ public class Test_L64Bm_simpleFast {
         public void doit() {
             bmTest001();
             maskValues();
-            rule30Test();
-            
-           // bmBase5_3();
-           // testCopy();
-           // testShift();
+            gobTest001();
+            //rule30Test();
+           
+        }
+        
+        public void gobTest001(){
+            String inGob="<BITMAP>\n"
+                    + "O O O X X X O X \n"
+                    + "X X O O O O X X \n"
+                    + "X X O X O X O X \n"
+                    + "O O X O X X X X \n"
+                    + "O O O X X O X X \n"
+                    + "O O O X O O O X \n"
+                    + "X X X O X X O X \n"
+                    + "O O O X X X X X \n";
+            long b0=inputString( inGob,0);
+            long b1=inputString( inGob,1);
+
+
+            assertEquals(outString(b0,b1),
+                    "<BITMAP>\n"
+                    + "O O O X X X O X \n"
+                    + "X X O O O O X X \n"
+                    + "X X O X O X O X \n"
+                    + "O O X O X X X X \n"
+                    + "O O O X X O X X \n"
+                    + "O O O X O O O X \n"
+                    + "X X X O X X O X \n"
+                    + "O O O X X X X X \n"
+            );
         }
         
         public void rule30Test(){
             long m=1L<<32;
             
-            for(int i=0;i<100;i++){
+            for(int i=0;i<1;i++){
                 m=rule30(m);
                 m=rule30(m);
-                System.out.printf("%64s\n",Long.toBinaryString(m));
+                //System.out.printf("%64s\n",Long.toBinaryString(m));
+                //System.out.println(""+outString(m, 'X', '-'));
             }
             
         }
@@ -83,242 +109,20 @@ public class Test_L64Bm_simpleFast {
                     + "X X X O X X O X \n"
                     + "O O O X X X X X \n"
             );
+            bm=setAt(bm, 3, 3, 1);
+            assertEquals(outString(bm, 'X', 'O'),
+                    "<BITMAP>\n"
+                    + "O O O X X X O X \n"
+                    + "X X O O O O X X \n"
+                    + "X X O X O X O X \n"
+                    + "O O X X X X X X \n"
+                    + "O O O X X O X X \n"
+                    + "O O O X O O O X \n"
+                    + "X X X O X X O X \n"
+                    + "O O O X X X X X \n"
+            );            
 
         }
-//
-//        public void bmBase5_3() {
-//
-//            RegBitMapsUtils x = new RegBitMapsUtils();
-//            IRegFactory rf = () -> new Reg64BitsBased(256);
-//            IRegBitMap bm = fact.alloc(5, 3);
-//
-//            x.intString(bm, 'X', 'O', "<BITMAP>\n"
-//                    + "OOOXX\n"
-//                    + "XXOOO\n"
-//                    + "XXOXO\n");
-//
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOXX\n"
-//                    + "XXOOO\n"
-//                    + "XXOXO\n"
-//            );
-//
-//        }
-//
-//        public void testCopy() {
-//
-//            RegBitMapsUtils x = new RegBitMapsUtils();
-//            IRegBitMap bm = fact.alloc(22, 9);
-//
-//            x.intString(bm, 'X', 'O', "<BITMAP>\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO");
-//
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n"
-//            );
-//            
-//            IRegBitMap bm2 = fact.alloc(5, 3);
-//
-//            x.intString(bm2, 'X', 'O', "<BITMAP>\n"
-//                    + "OOOXX\n"
-//                    + "XXOOO\n"
-//                    + "XXOXO\n");
-//
-//            assertEquals(x.outString(bm2, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOXX\n"
-//                    + "XXOOO\n"
-//                    + "XXOXO\n");
-//
-//            bm2.xor(bm2);
-//            bm2.cp(bm, 5, 2, 3, 2, 0, 0);
-//
-//            assertEquals(x.outString(bm2, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "XOXOO\n"
-//                    + "XXXOO\n"
-//                    + "OOOOO\n");
-//
-//            bm2.xor(bm2);
-//            bm2.cp(bm, 5, 2, 3, 2, 2, 1);
-//
-//            assertEquals(x.outString(bm2, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOOO\n"
-//                    + "OOXOX\n"
-//                    + "OOXXX\n"
-//            );
-//            
-//            bm.cp(bm2, 2, 1, 3, 2, 6, 3);
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXOXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n"
-//            );
-//        }
-//        
-//        
-//         public void testShift() {
-//            RegBitMapsUtils x = new RegBitMapsUtils();
-//            IRegBitMap bm = fact.alloc(22, 9);
-//
-//            x.intString(bm, 'X', 'O', "<BITMAP>\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n"
-//            );
-//            
-//            bm.shiftd();
-//
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"
-//                    + "OOOOOOOOOOOOOOOOOOOOOO\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    
-//            );     
-//            
-//            bm.shiftu();            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OOOOOOOOOOOOOOOOOOOOOO\n"
-//                    
-//            );              
-//            
-//            //====
-//            x.intString(bm, 'X', 'O', "<BITMAP>\n"
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n"                  
-//                );
-//            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXXO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXXO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n"  
-//            ); 
-//            
-//            bm.shiftr();
-//            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOOXXXOXXOXXXXOOXXXOO\n"
-//                    + "OXXOOOOXXOOXXXXXXXXXXO\n"
-//                    + "OXXOXOXOXXOXOXXOOXXOXX\n"
-//                    + "OOOXOXXXXXXOXOOXXOOXXO\n"
-//                    + "OOOOXXOXXXXOOOXOOOXXXO\n"
-//                    + "OOOOXOOOXOOOOXOOXXXOXO\n"
-//                    + "OXXXOXXOXOXXOOXOXOOXXO\n"
-//                    + "OOOOXXXXXXOOOOOXOXOOXX\n"
-//                    + "OOXXXOXXOOOOOXXXOOOOXO\n" 
-//            );             
-//            
-//            bm.shiftr();
-//            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOOOXXXOXXOXXXXOOXXXO\n"
-//                    + "OOXXOOOOXXOOXXXXXXXXXX\n"
-//                    + "OOXXOXOXOXXOXOXXOOXXOX\n"
-//                    + "OOOOXOXXXXXXOXOOXXOOXX\n"
-//                    + "OOOOOXXOXXXXOOOXOOOXXX\n"
-//                    + "OOOOOXOOOXOOOOXOOXXXOX\n"
-//                    + "OOXXXOXXOXOXXOOXOXOOXX\n"
-//                    + "OOOOOXXXXXXOOOOOXOXOOX\n"
-//                    + "OOOXXXOXXOOOOOXXXOOOOX\n" 
-//                    
-//            );          
-//            
-//            bm.shiftl();
-//            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOOXXXOXXOXXXXOOXXXOO\n"
-//                    + "OXXOOOOXXOOXXXXXXXXXXO\n"
-//                    + "OXXOXOXOXXOXOXXOOXXOXO\n"
-//                    + "OOOXOXXXXXXOXOOXXOOXXO\n"
-//                    + "OOOOXXOXXXXOOOXOOOXXXO\n"
-//                    + "OOOOXOOOXOOOOXOOXXXOXO\n"
-//                    + "OXXXOXXOXOXXOOXOXOOXXO\n"
-//                    + "OOOOXXXXXXOOOOOXOXOOXO\n"
-//                    + "OOXXXOXXOOOOOXXXOOOOXO\n" 
-//                    
-//            );              
-//               
-//            bm.shiftl();
-//            
-//            assertEquals(x.outString(bm, 'X', 'O'),
-//                    "<BITMAP>\n"                    
-//                    + "OOOXXXOXXOXXXXOOXXXOOO\n"
-//                    + "XXOOOOXXOOXXXXXXXXXXOO\n"
-//                    + "XXOXOXOXXOXOXXOOXXOXOO\n"
-//                    + "OOXOXXXXXXOXOOXXOOXXOO\n"
-//                    + "OOOXXOXXXXOOOXOOOXXXOO\n"
-//                    + "OOOXOOOXOOOOXOOXXXOXOO\n"
-//                    + "XXXOXXOXOXXOOXOXOOXXOO\n"
-//                    + "OOOXXXXXXOOOOOXOXOOXOO\n"
-//                    + "OXXXOXXOOOOOXXXOOOOXOO\n" 
-//                    
-//            );              
-//         }
     }// Testeur de bitMap
     
 }
