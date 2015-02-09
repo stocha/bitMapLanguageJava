fb1106aca945d0c30ab627f27f7f4c25cce1eba6
denis <denis@JahanTab>
2015-02-09 20:34:53 +0100

    timing sur des 381

--- src/v1/L64p/vrac/Bench_mesures.java
+++ src/v1/L64p/vrac/Bench_mesures.java
@@ -19,10 +19,44 @@ public class Bench_mesures {
         //timeRandomNoSuicideGameAccelerated();
         
         //timeSomeFinishAccel();
-        //time391RandHash();
-        time391RandomBit();
+        //time391RandHash(); time391RandHash();
+        //time391RandomBit(); time391RandomBit();
+        //timeBitSet();timeBitSet();
     }
     
+    
+    
+    public static void timeBitSet(){
+       Reg381 r[]=Reg381.allocBuff(10);
+       
+       Reg381 rh=Reg381.alloc();
+       Reg381 rbit=Reg381.alloc();
+       rh.setAt(190, 1);
+       long rand=1;
+       
+        final int nbGame = 6000000*4;    
+        
+        long t0 = System.nanoTime();   
+        double accCount=0;
+        
+        for(int i=0;i<nbGame;i++){
+            rh.randHash(r);
+            //accCount+=Math.abs(rh.count()-190.5);
+            rbit.setAt((int)(rand&0xFFFF%381), 1);
+            rand=L64fbase.rule30(rand);
+            //System.out.println("=================");
+            //System.out.println(g.debug_show());
+        }
+        
+       long t1 = System.nanoTime();
+
+        double t = (t1 - t0) / 1000000000.0;
+        System.out.println("timeBitSet " + nbGame + " actions en " + t + " secondes");
+        double nbgamSec = nbGame;
+        nbgamSec /= t;
+        System.out.println("" + nbgamSec + " actions par secondes");  
+        System.out.println("avg count = "+(accCount/(double) nbGame));        
+    }    
     
     public static void time391RandomBit(){
        Reg381 r[]=Reg381.allocBuff(10);
