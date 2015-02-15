fa3d5459093b0553dc04bad724aa52778275e93f
denis <denis@JahanTab>
2015-02-15 21:21:36 +0100

    Tentative fonctionnement.

--- src/v1/L64p/vrac/NumStruct64.java
+++ src/v1/L64p/vrac/NumStruct64.java
@@ -15,13 +15,18 @@ public class NumStruct64 {
 
     public long get(int pos) {
         long res = 0;
-        res |= ((d[5] >>> (63 - pos)) & 1L); res<<=1;
-        res |= ((d[4] >>> (63 - pos)) & 1L); res<<=1; 
-        res |= ((d[3] >>> (63 - pos)) & 1L); res<<=1; 
-        res |= ((d[2] >>> (63 - pos)) & 1L); res<<=1; 
-        res |= ((d[1] >>> (63 - pos)) & 1L); res<<=1; 
-        res |= ((d[0] >>> (63 - pos)) & 1L); 
-       
+        res |= ((d[5] >>> (63 - pos)) & 1L);
+        res <<= 1;
+        res |= ((d[4] >>> (63 - pos)) & 1L);
+        res <<= 1;
+        res |= ((d[3] >>> (63 - pos)) & 1L);
+        res <<= 1;
+        res |= ((d[2] >>> (63 - pos)) & 1L);
+        res <<= 1;
+        res |= ((d[1] >>> (63 - pos)) & 1L);
+        res <<= 1;
+        res |= ((d[0] >>> (63 - pos)) & 1L);
+
         return res;
     }
 
@@ -39,12 +44,90 @@ public class NumStruct64 {
     }
 
     public void clear() {
-        d[0] =0xAAAAAAAAAAAAAAAAL;
-        d[1] =0xCCCCCCCCCCCCCCCCL;
-        d[2] =0xF0F0F0F0F0F0F0F0L;
-        d[3] =0xFF00FF00FF00FF00L;
-        d[4] =0xFFFF0000FFFF0000L;
-        d[5] =0xFFFFFFFF00000000L;
+        d[0] = 0xAAAAAAAAAAAAAAAAL;
+        d[1] = 0xCCCCCCCCCCCCCCCCL;
+        d[2] = 0xF0F0F0F0F0F0F0F0L;
+        d[3] = 0xFF00FF00FF00FF00L;
+        d[4] = 0xFFFF0000FFFF0000L;
+        d[5] = 0xFFFFFFFF00000000L;
         //System.out.println(""+L64fbase.outString(d[1],0));
+    }
+    
+    public void findGroups(long a,long b){
+        clear();
+        while(groupStep(a,b)){
+        System.out.println("find group\n" + this.out());
+        }
+    }
+
+    public boolean groupStep(long a, long b) {
+        long isH = 0;
+        long hasH = 0;
+
+        long lay;
+        long dir;
+        
+        long cond=0;
+
+        for (int i = 0; i < d.length; i++) {
+            lay = d[i];
+            dir = d[i] >>> 8;
+            isH = isH | (dir & ~lay);
+        }
+        for (int i = 0; i < d.length; i++) {
+            dir = d[i] >>> 8;
+            cond=isH&(a&(a>>>8));
+            d[i] &= ~(cond);
+            d[i] |= dir & cond;
+        }
+        hasH |= cond;
+        isH = 0;
+
+        for (int i = 0; i < d.length; i++) {
+            lay = d[i];
+            dir = d[i] << 8;
+            isH = isH | (dir & ~lay);
+        }
+        for (int i = 0; i < d.length; i++) {
+            dir = d[i] << 8;
+            cond=isH&(a&(a<<8));
+            d[i] &= ~(cond);
+            d[i] |= dir & cond;
+        }
+        hasH |= cond;
+        isH = 0;
+
+        for (int i = 0; i < d.length; i++) {
+            lay = d[i];
+            dir = d[i] << 1 & L64fbase.LMASK;
+            isH = isH | (dir & ~lay);
+        }
+        for (int i = 0; i < d.length; i++) {
+            dir = d[i] << 1 & L64fbase.LMASK;
+            cond=isH&(a&(a<<1)&L64fbase.LMASK);
+            d[i] &= ~(cond);
+            d[i] |= dir & cond;
+        }
+        hasH |= cond;
+        isH = 0;
+
+        for (int i = 0; i < d.length; i++) {
+            lay = d[i];
+            dir = d[i] >>> 1 & L64fbase.RMASK;
+            isH = isH | (dir & ~lay);
+        }
+        for (int i = 0; i < d.length; i++) {
+            dir = d[i] >>> 1 & L64fbase.RMASK;
+            cond=isH&(a&(a>>>1)&L64fbase.RMASK);
+            d[i] &= ~(cond);
+            d[i] |= dir & cond;
+        }
+       
+        isH = 0;
+        hasH |= cond;
+
+        //System.out.println(""+" "+L64fbase.outString(isH,0));
+        return (hasH != 0L);
+
     }
 }
--- src/v1/L64p/vrac/Test_NumStruct64.java
+++ src/v1/L64p/vrac/Test_NumStruct64.java
@@ -10,14 +10,28 @@ package v1.L64p.vrac;
  * @author denis
  */
 public class Test_NumStruct64 {
-        public static void main(String args[]) {
-            test001();
+
+    public static void main(String args[]) {
+        test001();
+    }
+
+    public static void test001() {
+        NumStruct64 ns = new NumStruct64();
+        ns.clear();
+
+        System.out.println("before\n" + ns.out());        
+        
+        while (ns.groupStep(0, 0)) {
+            System.out.println("steping\n" + ns.out());
         }
         
-        public static void test001(){
-            NumStruct64 ns=new NumStruct64();
-            ns.clear();
-            
-            System.out.println(""+ns.out());
-        }
+        L64fbase.gob64Struct gob=new L64fbase.gob64Struct();
+        gob.init();
+        gob.finishRandNoSuicide(0.0, 0);
+        System.out.println(""+gob.debug_show());
+        ns.findGroups(gob.p0, gob.p1);
+        System.out.println("groups\n" + ns.out());   
+        
+        
+    }
 }
